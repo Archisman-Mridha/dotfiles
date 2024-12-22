b87397dc4fff15448d7dad12555ef65a666d52c6
Archisman-Mridha <archismanmridha12345@gmail.com>
2024-12-23 00:44:51 +0530

    fixing vim clipboard issue (again :))
    Signed-off-by: Archisman-Mridha <archismanmridha12345@gmail.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -35,3 +35,27 @@ require("which-key").setup({
 
 -- Configure lualine to evil mode.
 require("config.lualine")
+
+-- For `no clipboard response from terminal` Neovim issue in an SSH session.
+-- REFER : https://www.reddit.com/r/neovim/comments/1e9vllk/neovim_weird_issue_when_copypasting_using_osc_52/.
+
+vim.o.clipboard = "unnamedplus"
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
